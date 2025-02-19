0bca8fb385b75d65f39dc4d78b8717e18bbfdcf7
Yahoo <yahya.a002@outlook.com>
2025-02-19 18:09:14 +0530

    Folder structure complete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ArogyaSahayak',
+    version= '0.0.0',
+    author= 'Yahya Aseerullah',
+    author_email= 'yahyaaseerullah002@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
